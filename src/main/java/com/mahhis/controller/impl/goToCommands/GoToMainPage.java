614125f8df656bea05d0ce7da32250dc2ebf3522
Ivan <vasiukovich03@gmail.com>
2023-01-11 16:09:46 +0300

    finalV2

--- src/main/java/com/mahhis/controller/impl/goToCommands/GoToMainPage.java
+++ src/main/java/com/mahhis/controller/impl/goToCommands/GoToMainPage.java
@@ -1,8 +1,9 @@
 package com.mahhis.controller.impl.goToCommands;
 
 import com.mahhis.controller.Command;
-import com.mahhis.controller.constants.CommandName;
+import com.mahhis.controller.constants.PagePath;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,9 +14,9 @@ public class GoToMainPage implements Command {
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        response.sendRedirect(CommandName.MAIN_PAGE);
-       /* RequestDispatcher dispatcher = request.getRequestDispatcher(PagePath.MAIN_PAGE);
-        dispatcher.forward(request, response);*/
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(PagePath.MAIN_PAGE);
+        dispatcher.forward(request, response);
 
     }
 }
