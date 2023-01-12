38a2ab120306fa9a83ef479092cdfcc98eaff6a1
Ivan <vasiukovich03@gmail.com>
2023-01-12 12:57:15 +0300

    finalV2

--- src/main/java/com/mahhis/controller/constants/CommandName.java
+++ src/main/java/com/mahhis/controller/constants/CommandName.java
@@ -3,7 +3,7 @@ package com.mahhis.controller.constants;
 public class CommandName {
 
     public static final String UTF_8 = "UTF-8";
-    public static final String PAYMENT_SYSTEM_CONTROLLER = "/PaymentSystem/Controller?";
+    public static final String PAYMENT_SYSTEM_CONTROLLER = "/Controller?";
     //public static final String ERROR_PAGE = PAYMENT_SYSTEM_CONTROLLER + "command=GO_TO_ERROR_PAGE";
     public static final String MAIN_PAGE = PAYMENT_SYSTEM_CONTROLLER + "command=GoToMainPage";
     public static final String TRANSACTION_LIST_PAGE = PAYMENT_SYSTEM_CONTROLLER + "command=GoToTransactionListPage";
